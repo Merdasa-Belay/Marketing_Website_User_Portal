5b0266de9e82448f767b8494b4cd49411c63d12a
Merdasa-Belay <merdasabelay@gmail.com>
2024-08-12 22:08:49 +0300

    update

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -18,4 +18,9 @@ class Customer extends Model
         'password',
         'confirmpassword'
     ];
+
+    public function properties()
+    {
+        return $this->hasMany(Property::class);
+    }
 }
---
+++ database/migrations/2024_08_12_151519_create_properties_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->text('name');
+            $table->text('description');
+            $table->float('price');
+            $table->timestamps();
+            $table->foreign("customer_id")->references('id')->on('customers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
