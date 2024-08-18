31c3b767f5324143ae453c8534f2bc85f1ac5c85
Merdasa-Belay <merdasabelay@gmail.com>
2024-08-18 14:43:31 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,32 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CustomerController;
 use App\Models\Customer;
 use Illuminate\Support\Facades\Route;
-
+// Registration page
 Route::get('/customer_register', [CustomerController::class, "registration"]);
+
+
+
+// Login page
 Route::get('/customer_login', [CustomerController::class, "login_page"]);
 
 
 Route::resource('/customers', CustomerController::class);
-Route::get('/my_detail', [CustomerController::class, "myDetail"]);
-Route::get('/my_details', [CustomerController::class, 'myDetails']);
+
+
+// user profile
+
+Route::get('/my-detail', [CustomerController::class, 'myDetail'])->name('my_detail');
+
+// dashboard
+Route::get('/my-dashboard', [CustomerController::class, 'myDashboard'])->name('my_dashboard');
+
+// reports
+
+Route::get('/my-reports', [CustomerController::class, 'myReports'])->name('my_reports');
+
+
+// Datasets
+
+
+
+Route::get('/my-datasets', [CustomerController::class, 'myDatasets'])->name('my_datasets');
