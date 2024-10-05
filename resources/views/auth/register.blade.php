2f9528b44d3721da1f974cdd28c4574481e6fb87
Merdasa-Belay <merdasabelay@gmail.com>
2024-10-05 21:02:03 +0300

    update countries selection

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,15 +13,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Stylesheet -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/register.css') }}">
     <!-- Font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
 <body>
     <!-- Datadudu image -->
-    <div id="image-dudu">
-        <img id="datadudu-image" src="{{ asset('assets/profile_pic/dascena.png') }}" alt="">
+    <div id="image-dascena">
+        <img id="dascena-image" src="{{ asset('assets/profile_pic/dascena.png') }}" alt="">
     </div>
 
 
@@ -125,5 +125,6 @@
 <script>
     document.addEventListener("DOMContentLoaded", showCountry);
 </script>
+
 
 </html>
