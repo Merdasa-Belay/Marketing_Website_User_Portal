bed120695b9fda300303a6901043b2a01357f3ff
Merdasa-Belay <merdasabelay@gmail.com>
2024-10-10 20:28:25 +0300

    update table

--- resources/views/layouts/transaction-table.blade.php
+++ resources/views/layouts/transaction-table.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="{{ asset('assets/css/transaction.css') }}">
 
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 {{-- transaction table --}}
 
 <div class="container mt-5 transaction-buttons">
